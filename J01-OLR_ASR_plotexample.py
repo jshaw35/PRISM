e1761779b1c3eb0497b89e945794bee7f922b620
jshaw35 <jonah.k.shaw@gmail.com>
2026-03-12 15:49:59 -0600

    manual merge with Leah's improvements

--- J01-OLR_ASR_plotexample.py
+++ J01-OLR_ASR_plotexample.py
@@ -7,12 +7,12 @@ import seaborn as sns
 
 # %%
 def plot_radiative_imbalance(
-    olr_ds,
-    asr_ds,
+    olr_da,
+    asr_da,
     ax=None,
     plot_kwargs=None,
     fontsize=14,
-    cmap=sns.color_palette("viridis", as_cmap=True),
+    cmap=mpl.colormaps['viridis'],
 ):
     if ax is None:
         fig, ax = plt.subplots(figsize=(10, 5))
@@ -22,25 +22,32 @@ def plot_radiative_imbalance(
 
     # Plot OLR vs ASR with color gradient for time dimension
     ax.plot(
-        olr_ds,
-        asr_ds,
+        olr_da,
+        asr_da,
         color="black",
         alpha=0.5,
         zorder=5,
         linewidth=0.5,
     )
     scatter = ax.scatter(
-        olr_ds,
-        asr_ds,
-        c=np.arange(len(olr_ds)),
+        olr_da,
+        asr_da,
+        c=olr_da['time.year']+0.083*olr_da['time.month'], #fractional year coordinate
         cmap=cmap,
         zorder=10,
         **plot_kwargs,
     )
 
     # Add a colorbar with discrete intervals and extend='both' keyword
-    bounds = np.arange(olr_ds["time"].min().item(), olr_ds["time"].max().item() + 1, max(1, len(olr_ds)//10))   
-    norm = mpl.colors.BoundaryNorm(bounds, cmap.N, extend='both')
+    # bounds = pd.date_range(
+    #     start=pd.to_datetime(olr_da["time"].min().data),
+    #     end=pd.to_datetime(olr_da["time"].max().data), 
+    #     periods=min(olr_da.sizes['time'],255)) 
+    bounds = np.arange(
+        olr_da['time.year'].min(),
+        olr_da['time.year'].max(),
+        max(1,(olr_da['time.year'].max()-olr_da['time.year'].min())/255))
+    norm = mpl.colors.BoundaryNorm(np.array(bounds), cmap.N, extend='both')
 
     plt.colorbar(
         mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
@@ -49,8 +56,8 @@ def plot_radiative_imbalance(
     )
 
     # Plot the 1:1 line
-    min_val = min(olr_ds.min().item(), asr_ds.min().item())
-    max_val = max(olr_ds.max().item(), asr_ds.max().item())
+    min_val = min(olr_da.min().item(), asr_da.min().item())
+    max_val = max(olr_da.max().item(), asr_da.max().item())
     ax.plot(
         [min_val, max_val],
         [min_val, max_val],
@@ -59,8 +66,8 @@ def plot_radiative_imbalance(
         zorder=0,
     )
     ax.set_aspect("equal", adjustable="box")
-    ax.set_xlabel("OLR (Wm$^{-2}$)", fontsize=fontsize)
-    ax.set_ylabel("ASR (Wm$^{-2}$)", fontsize=fontsize)
+    ax.set_xlabel("OLR [Wm$^{-2}$]", fontsize=fontsize)
+    ax.set_ylabel("ASR [Wm$^{-2}$]", fontsize=fontsize)
 
     if fig is None:
         return ax
@@ -129,23 +136,28 @@ def get_weights_by_month(
 
 # %%
 
-olr = np.arange(0, 10, 0.5)  # Example ASR values
-asr = 0.5 * (olr**2)
 
-fig, ax = plot_radiative_imbalance(
-    olr_ds=xr.DataArray(olr, dims=["time"], coords={"time": np.arange(len(olr))}),
-    asr_ds=xr.DataArray(asr, dims=["time"], coords={"time": np.arange(len(asr))}),
-    plot_kwargs={"marker": "o", "linestyle": "solid"},
-)
-
-# Get rid of the plot edges and add gridlines through the origin
-ax.spines["top"].set_visible(False)
-ax.spines["bottom"].set_visible(False)
-ax.spines["right"].set_visible(False)
-ax.spines["left"].set_visible(False)
-ax.axhline(0, color="grey", linestyle="solid", linewidth=1.5)
-ax.axvline(0, color="grey", linestyle="solid", linewidth=1.5)
-ax.grid(True, which="both", linestyle="--", linewidth=0.5)
-
+def main():
+    olr = np.arange(0, 10, 0.5)  # Example ASR values
+    asr = 0.5 * (olr**2)
+    
+    fig, ax = plot_radiative_imbalance(
+        olr_da=xr.DataArray(olr, dims=["time"], coords={"time": np.arange(len(olr))}),
+        asr_da=xr.DataArray(asr, dims=["time"], coords={"time": np.arange(len(asr))}),
+        plot_kwargs={"marker": "o", "linestyle": "solid"},
+    )
+    
+    # Get rid of the plot edges and add gridlines through the origin
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.axhline(0, color="grey", linestyle="solid", linewidth=1.5)
+    ax.axvline(0, color="grey", linestyle="solid", linewidth=1.5)
+    ax.grid(True, which="both", linestyle="--", linewidth=0.5)
+    
 # %%
+
+if __name__ == '__main__':
+    main()
 
