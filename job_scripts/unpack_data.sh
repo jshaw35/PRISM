45424f2c4aec91959030befdb8b719cbf1800582
jshaw35 <jonah.k.shaw@gmail.com>
2026-05-11 14:01:02 -0600

    Update unpacking script for new spatial and OHC data

--- job_scripts/unpack_data.sh
+++ job_scripts/unpack_data.sh
@@ -14,6 +14,7 @@ unzip -n zipped_data/CESM2_WACCM_HIST.zip -d data/
 
 # Control baselines
 unzip -n zipped_data/control_baselines.zip -d data
+unzip -n zipped_data/control_baselines_ohc.zip -d data
 
 # NMSE error components
 unzip -n zipped_data/CESM2_LME_control_error_components.zip -d data/
@@ -23,3 +24,15 @@ unzip -n zipped_data/CESM2_LE_2000_2009_cmip6_error_components.zip -d data/
 unzip -n zipped_data/CESM2_WACCM_1850control_error_components.zip -d data/
 unzip -n zipped_data/CESM2_WACCM_HIST_1850_1864_error_components.zip -d data/
 unzip -n zipped_data/CESM2_WACCM_HIST_2000_2014_error_components.zip -d data/
+
+# Spatial data
+unzip -n zipped_data/ARISE-SAI_spatialdata.zip -d data/
+unzip -n zipped_data/ARISE-SAI_spatialdata_ohc.zip -d data/
+unzip -n zipped_data/CESM2_WACCM_1850control_spatialdata1.zip -d data/
+unzip -n zipped_data/CESM2_WACCM_1850control_spatialdata2.zip -d data/
+unzip -n zipped_data/CESM2_WACCM_1850control_spatialdata3.zip -d data/
+unzip -n zipped_data/CESM2_WACCM_1850control_spatialdata_ohc.zip -d data/
+unzip -n zipped_data/CESM2_WACCM_SSP2-4.5_MCB_spatialdata.zip -d data/
+unzip -n zipped_data/CESM2_WACCM_SSP2-4.5_MCB_spatialdata_ohc.zip -d data/
+unzip -n zipped_data/CESM2_WACCM_SSP2-4.5_spatialdata.zip -d data/
+unzip -n zipped_data/CESM2_WACCM_SSP2-4.5_spatialdata_ohc.zip -d data/
