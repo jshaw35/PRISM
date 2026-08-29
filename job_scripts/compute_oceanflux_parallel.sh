dcbda8a836aa9f1a7c7154d21ca66c0d9612f6af
jshaw35 <jonah.k.shaw@gmail.com>
2026-08-29 06:33:59 -0600

    Update bash scripts for ocean preprocessing to run on glade

--- job_scripts/compute_oceanflux_parallel.sh
+++ job_scripts/compute_oceanflux_parallel.sh
@@ -2,11 +2,16 @@
 #PBS -N ohf_compute
 #PBS -A UCUC0007
 #PBS -l select=1:ncpus=1:mem=8GB:ngpus=0
-#PBS -l walltime=0:05:00
+#PBS -l walltime=0:15:00
 #PBS -q casper
-#PBS -j oe
+#PBS -o /glade/u/home/jonahshaw/Scripts/git_repos/PRISM/job_logs/
+#PBS -e /glade/u/home/jonahshaw/Scripts/git_repos/PRISM/job_logs/
 
 # Kill all casper jobs with: qselect -u jonahshaw | xargs qdel
 cd /glade/u/home/jonahshaw/Scripts/git_repos/PRISM/job_logs
-conda activate /glade/work/jonahshaw/conda-envs/hackathon_extended
-python /glade/u/home/jonahshaw/Scripts/git_repos/PRISM/J09_oceanflux_calculation_parallel.py
+echo "Job started at $(date)"
+
+mamba run -p /glade/work/jonahshaw/conda-envs/hackathon_extended \
+    python /glade/u/home/jonahshaw/Scripts/git_repos/PRISM/J09_oceanflux_calculation_parallel.py
+
+echo "Job completed successfully at $(date)"
